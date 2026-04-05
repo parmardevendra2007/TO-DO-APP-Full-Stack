34e1bbb43376553606ff71aee19a75e9a5e25c55
parmardevendra2007 <parmaedevendra2007@gmail.com>
2026-04-05 11:49:58 +0530

    fix cors for production

--- Backend/app.js
+++ Backend/app.js
@@ -12,7 +12,7 @@ const app = express();
 // Middleware
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://sweet-mermaid-cce03a.netlify.app",
     credentials: true,
   })
 );
