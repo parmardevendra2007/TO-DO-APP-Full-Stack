fcdb36c002857e05481b2b7cffecc7f5c6829d04
parmardevendra2007 <parmaedevendra2007@gmail.com>
2026-04-05 22:09:38 +0530

    cors origin set

--- Backend/app.js
+++ Backend/app.js
@@ -12,7 +12,7 @@ const app = express();
 // Middleware
 app.use(
   cors({
-    origin: "https://sweet-mermaid-cce03a.netlify.app",
+    origin: "https://to-do-app-full-stack-ume7.vercel.app/register",
     credentials: true,
   })
 );
