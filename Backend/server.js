7697c6e146795700d781a33a7af27e552c7ab42b
parmardevendra2007 <parmaedevendra2007@gmail.com>
2026-04-05 21:28:35 +0530

    fix: port binding for render

--- Backend/server.js
+++ Backend/server.js
@@ -7,6 +7,6 @@ connectDB();
 console.log("MONGO_URI:", process.env.MONGO_URI);
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
+app.listen(PORT,'0.0.0.0', () => {
   console.log(`Server is running on port ${PORT}`);
 });
